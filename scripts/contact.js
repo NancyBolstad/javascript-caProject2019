3005bc970e0238a3f3adb2f4aea1436b826e219c
NancyBolstad <litchacha@gmail.com>
2019-02-27 21:38:53 +0100

    Initial commit

---
+++ scripts/contact.js
@@ -0,0 +1,64 @@
+// refer to question 4 before development starts for scope document
+
+//Form validation only happens after a user clicks the submit button.
+document.getElementById("submitContact").addEventListener("click", function validateForm() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    //RFC2822 standards email validation, refer to:https://regexr.com/2rhq7
+    var emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    //Regular expression for special number format: xxx xxx xxxx OR xxx-xxx-xxxx OR xxx.xxx.xxxx
+    var phoneRegex = /(\d{3}\s\d{3}\s\d{4})|(\d{3}\.\d{3}\.\d{4})|(\d{3}-\d{3}-\d{4})/g;
+
+
+    //Validate name, last name, email and phone numbers and display display error messages if there is an issue with the values for each textbox respectively. If the value of the textbox is valid, hide the error message.  
+    function checkFirstName() {
+        if (firstName == "" || firstName == null) {
+            document.getElementById("firstNameError").style.display = "block";
+            document.getElementById("firstName").focus();
+            return false;
+        } else {
+            document.getElementById("firstNameError").style.display = "none";
+            return true;
+        }
+    }
+
+    function checkLastName() {
+        if (lastName == "" || lastName == null) {
+            document.getElementById("lastNameError").style.display = "block";
+            document.getElementById("lastName").focus();
+            return false;
+        } else {
+            document.getElementById("lastNameError").style.display = "none";
+            return true;
+        }
+    }
+
+    function checkPhone() {
+        if (!phoneRegex.test(phone)) {
+            document.getElementById("phoneError").style.display = "block";
+            document.getElementById("phone").focus();
+            return false;
+        } else {
+            document.getElementById("phoneError").style.display = "none";
+            return true;
+        }
+    }
+
+    function checkEmail() {
+        if (!emailRegex.test(email)) {
+            document.getElementById("emailError").style.display = "block";
+            document.getElementById("email").focus();
+            return false;
+        } else {
+            document.getElementById("emailError").style.display = "none";
+            return true;
+        }
+    }
+
+    //Only submit the form when all input has been validated.
+    if (checkFirstName() && checkLastName() && checkPhone() && checkEmail()) {
+        alert("Great! Your contact information has been validated. Form has been submitted.");
+    }
+});
---
+++ scripts/script.js
@@ -0,0 +1,100 @@
+const apiURL = "https://api.magicthegathering.io/v1/cards";
+const container = document.getElementById("cards");
+
+makeAPICall(generateCards);
+
+
+//Reusable API calling function for executing multiple AJAX tasks, consisting of a function that executes the XMLHttpRequest object, and one callback function for each AJAX task. Refer to: https://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp
+function makeAPICall(callback) {
+  //Connect to the URL provided
+  var request = new XMLHttpRequest();
+  request.open("GET", apiURL, true);
+
+  request.onload = function () {
+    //Convert the results to JSON format
+    var data = JSON.parse(request.response);
+    if (request.status >= 200 && request.status < 400) {
+      //Call the function from the method that returns the JSON data and pass in the JSON data.
+      callback(data.cards);
+    } else {
+      displayErrorInnerHTML("Gah, something went wrong!");
+    }
+  };
+  request.send();
+}
+
+//Create a function, that takes the JSON Object as an argument.
+function generateCards(jsonObject) {
+  //Used the following keys to create cards: name, imageUrl and id.
+  jsonObject.forEach(function (element) {
+    const newCard = document.createElement("div");
+    newCard.setAttribute("class", "col-sm-4");
+
+    const newCardContainer = document.createElement("div");
+    newCardContainer.setAttribute("class", "card-container");
+
+    const h4 = document.createElement("h4");
+    h4.textContent = element.name;
+
+    const cardIMG = document.createElement("img");
+    cardIMG.style.width = "100%";
+    //Test to see if the value is undefined, if the value is undefined, please use a placeholder image instead.
+    if (typeof element.imageUrl === "undefined") {
+      cardIMG.src = "https://via.placeholder.com/223x310";
+    } else {
+      cardIMG.src = element.imageUrl;
+    }
+
+    const viewMore = document.createElement("a");
+    const cardID = element.id;
+    viewMore.setAttribute("class", "btn btn-success");
+    viewMore.textContent = "View More";
+    //Passed in id as a query string in the URL.
+    viewMore.href = "card-specific.html?id=" + cardID;
+
+    //Append all the data to the div with the “cards” ID attached to it.
+    container.appendChild(newCard);
+    newCard.appendChild(newCardContainer);
+    newCardContainer.appendChild(h4);
+    newCardContainer.appendChild(cardIMG);
+    newCardContainer.appendChild(viewMore);
+
+  });
+}
+
+//The search button will trigger a click event.   
+document.getElementById("searchButton").addEventListener("click", function () {
+  if (document.getElementById("search").value == "" || document.getElementById("search").value == null) {
+    alert("Input cannot be empty, try again!");
+  } else {
+    //First, delete all the HTML inside the div that has the ID “cards” attached to it.
+    while (container.hasChildNodes()) {
+      container.removeChild(container.firstChild);
+    }
+    //Make a call to the api get all the results back
+    makeAPICall(cardFilter);
+  }
+});
+
+function cardFilter(jsonObject){
+  //get the value from the search text box
+  const userInput=document.getElementById("search").value;
+  //Filter through all the results by the value that was in the textbox. If it finds any results it should be added to a new array and the new array.
+  const result=jsonObject.filter(function(element){
+      return (element.name==userInput);
+  });
+
+  //Display the searching result. If the application doesn’t find any results a suitable message should be displayed.
+  if(result.length>0){
+    generateCards(result);
+  }else{
+    displayErrorInnerHTML("Card does not exist.");
+  }
+}
+
+function displayErrorInnerHTML(msg) {
+  const errorMessage = document.createElement("h1");
+  errorMessage.style.color = "red";
+  errorMessage.textContent = msg;
+  container.appendChild(errorMessage);
+}
