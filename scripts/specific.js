f42922b48a73eaa3c807d98e3a11bb67af297cdb
NancyBolstad <litchacha@gmail.com>
2019-05-01 17:42:18 +0200

    Use window.location to redirect url

--- scripts/specific.js
+++ scripts/specific.js
@@ -1,53 +1,65 @@
 // refer to question 2 before development starts for scope document
 'use strict';
 
-(async function getSpecific() {
+function hasQueryString() {
+  const pageParams = new URLSearchParams(window.location.search);
+  if (!pageParams.toString()) {
+    alert('Oops!');
+    window.location = './index.html';
+  } else {
+    const url = `https://api.magicthegathering.io/v1/cards/${pageParams.get(
+      'id'
+    )}`;
+    fetchCard(url, showCard);
+  }
+}
+
+async function fetchCard(url, doSomeThing) {
   try {
-    const pageParams = new URLSearchParams(window.location.search);
-    if (!pageParams.toString()) {
-      alert('Oops! There is no query string!');
-    } else {
-      const data = await (await fetch(
-        `https://api.magicthegathering.io/v1/cards/${pageParams.get('id')}`
-      )).json();
-      const imgContainer = document.getElementById('cardImage');
-      const cardDetailsContainer = document.getElementById('cardDetails');
-
-      //Append the image to the div called “cardImage”, set the width of the image to 100%.
-      const cardIMG = document.createElement('img');
-      cardIMG.style.width = '100%';
-      //Test to see if the imageUrl value is undefined, if the value is undefined, use the placeholder image instead
-      if (typeof data.card.imageUrl != 'undefined') {
-        cardIMG.src = data.card.imageUrl;
-      } else {
-        cardIMG.src = 'https://via.placeholder.com/223x310';
-      }
-
-      const h2 = document.createElement('h2');
-      h2.textContent = data.card.name;
-
-      const about = document.createElement('div');
-      about.innerHTML = `<b>About: </b>${data.card.text}`;
-
-      const rarity = document.createElement('div');
-      rarity.innerHTML = `<b>Rarity: </b> ${data.card.rarity}`;
-
-      const colors = document.createElement('div');
-      colors.innerHTML = `<b>Colors: </b>${data.card.colors}`;
-
-      const returnLink = document.createElement('a');
-      returnLink.innerHTML = 'Back to the home page >>';
-      returnLink.setAttribute('href', './index.html');
-
-      // The card template followed the original code inside the card-specific.html file
-      imgContainer.appendChild(cardIMG);
-      cardDetailsContainer.appendChild(h2);
-      cardDetailsContainer.appendChild(about);
-      cardDetailsContainer.appendChild(rarity);
-      cardDetailsContainer.appendChild(colors);
-      cardDetailsContainer.appendChild(returnLink);
-    }
+    const data = await (await fetch(url)).json();
+    doSomeThing(data.card);
   } catch (error) {
     showMessage('Something went wrong!');
   }
-})();
+}
+
+function showCard(card) {
+  const imgContainer = document.getElementById('cardImage');
+  const cardDetailsContainer = document.getElementById('cardDetails');
+
+  //Append the image to the div called “cardImage”, set the width of the image to 100%.
+  const cardIMG = document.createElement('img');
+  cardIMG.style.width = '100%';
+  //Test to see if the imageUrl value is undefined, if the value is undefined, use the placeholder image instead
+  if (typeof card.imageUrl != 'undefined') {
+    cardIMG.src = card.imageUrl;
+  } else {
+    cardIMG.src = 'https://via.placeholder.com/223x310';
+  }
+
+  const h2 = document.createElement('h2');
+  h2.textContent = card.name;
+
+  const about = document.createElement('div');
+  about.innerHTML = `<b>About: </b>${card.text}`;
+
+  const rarity = document.createElement('div');
+  rarity.innerHTML = `<b>Rarity: </b> ${card.rarity}`;
+
+  const colors = document.createElement('div');
+  colors.innerHTML = `<b>Colors: </b>${card.colors}`;
+
+  const returnLink = document.createElement('a');
+  returnLink.innerHTML = 'Back to the home page >>';
+  returnLink.setAttribute('href', './index.html');
+
+  // The card template followed the original code inside the card-specific.html file
+  imgContainer.appendChild(cardIMG);
+  cardDetailsContainer.appendChild(h2);
+  cardDetailsContainer.appendChild(about);
+  cardDetailsContainer.appendChild(rarity);
+  cardDetailsContainer.appendChild(colors);
+  cardDetailsContainer.appendChild(returnLink);
+}
+
+hasQueryString();
