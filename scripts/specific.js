e889d050bd8621550495914996f5587f6a38e5fd
NancyBolstad <litchacha@gmail.com>
2019-05-01 15:19:15 +0200

    Use URLSearchParams to get query string, apply ES2016/17 syntax

--- scripts/specific.js
+++ scripts/specific.js
@@ -1,68 +1,53 @@
 // refer to question 2 before development starts for scope document
-"use strict";
+'use strict';
 
-// get URL query string
-function getQueryStringValue(key) {
-    return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-}
-
-function getSpecific() {
-    // const variable for the id
-    const id = getQueryStringValue("id");
-
-    // Take the id constiable, make an API call, but append the ID to end of the API Call.
-    const request = new XMLHttpRequest();
-    request.open("GET", "https://api.magicthegathering.io/v1/cards/" + id, true);
-    request.onload = function () {
-        const data = JSON.parse(request.response);
-        //Get the following keys: imageUrl, name, text, rarity and colors. Display their values.
-        if (request.status >= 200 && request.status < 400) {
-            const imgContainer = document.getElementById("cardImage");
-            const cardDetailsContainer = document.getElementById("cardDetails");
-
-            //Append the image to the div called “cardImage”, set the width of the image to 100%.
-            const cardIMG = document.createElement("img");
-            cardIMG.style.width = "100%";
-            //Test to see if the imageUrl value is undefined, if the value is undefined, use the placeholder image instead
-            if (typeof data.card.imageUrl != "undefined") {
-                cardIMG.src = data.card.imageUrl;
-            } else {
-                cardIMG.src = "https://via.placeholder.com/223x310";
-            }
-
-            const h2 = document.createElement("h2");
-            h2.textContent = data.card.name;
-
-            const about = document.createElement("div");
-            about.innerHTML = "<b>About: </b>" + data.card.text;
-
-            const rarity = document.createElement("div");
-            rarity.innerHTML = "<b>Rarity: </b>" + data.card.rarity;
-
-            const colors = document.createElement("div");
-            colors.innerHTML = "<b>Colors: </b>" + data.card.colors;
-
-            // The card template followed the original code inside the card-specific.html file 
-            imgContainer.appendChild(cardIMG);
-            cardDetailsContainer.appendChild(h2);
-            cardDetailsContainer.appendChild(about);
-            cardDetailsContainer.appendChild(rarity);
-            cardDetailsContainer.appendChild(colors);
-        } else {
-            alert("Request failed.");
-        }
-    };
-    request.send();
-}
-
-// Test to see if there is no query string. If there isn't, display a suitable message.
-function testQueryStringValue() {
-    const queryString = window.location.search;
-    if (!queryString.toString()) {
-        alert("Oops! There is no query string!");
+(async function getSpecific() {
+  try {
+    const pageParams = new URLSearchParams(window.location.search);
+    if (!pageParams.toString()) {
+      alert('Oops! There is no query string!');
     } else {
-        getSpecific();
-    }
-}
+      const data = await (await fetch(
+        `https://api.magicthegathering.io/v1/cards/${pageParams.get('id')}`
+      )).json();
+      const imgContainer = document.getElementById('cardImage');
+      const cardDetailsContainer = document.getElementById('cardDetails');
 
-testQueryStringValue();
+      //Append the image to the div called “cardImage”, set the width of the image to 100%.
+      const cardIMG = document.createElement('img');
+      cardIMG.style.width = '100%';
+      //Test to see if the imageUrl value is undefined, if the value is undefined, use the placeholder image instead
+      if (typeof data.card.imageUrl != 'undefined') {
+        cardIMG.src = data.card.imageUrl;
+      } else {
+        cardIMG.src = 'https://via.placeholder.com/223x310';
+      }
+
+      const h2 = document.createElement('h2');
+      h2.textContent = data.card.name;
+
+      const about = document.createElement('div');
+      about.innerHTML = `<b>About: </b>${data.card.text}`;
+
+      const rarity = document.createElement('div');
+      rarity.innerHTML = `<b>Rarity: </b> ${data.card.rarity}`;
+
+      const colors = document.createElement('div');
+      colors.innerHTML = `<b>Colors: </b>${data.card.colors}`;
+
+      const returnLink = document.createElement('a');
+      returnLink.innerHTML = 'Back to the home page >>';
+      returnLink.setAttribute('href', './index.html');
+
+      // The card template followed the original code inside the card-specific.html file
+      imgContainer.appendChild(cardIMG);
+      cardDetailsContainer.appendChild(h2);
+      cardDetailsContainer.appendChild(about);
+      cardDetailsContainer.appendChild(rarity);
+      cardDetailsContainer.appendChild(colors);
+      cardDetailsContainer.appendChild(returnLink);
+    }
+  } catch (error) {
+    showMessage('Something went wrong!');
+  }
+})();
